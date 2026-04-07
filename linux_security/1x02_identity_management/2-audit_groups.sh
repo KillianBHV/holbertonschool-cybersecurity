6551d88009ce7778d4f60915d48540fa9a54ac2e
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-07 02:32:46 -0400

    Total Recoding - Too complicated previous

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 GROUPS="docker disk shadow"
 
-declare -A GID_TO_GROUPS
-while IFS=: read -r name _ gid _; do
-	for target in $GROUPS; do
-		if [ "$name" = "$target" ]; then
-			GID_TO_GROUPS["$gid"]="$name"
-		fi
-	done
-done < <(getent group)
-
-awk -F: '($3 >= 1000) {print $1 ":" $4}' "$1" | while read -r user gid; do
-	if [ -n "${GID_TO_GROUPS[$gid]}" ]; then
-		echo "$user:${GID_TO_GROUPS[$gid]}"
-	fi
-
+awk -F: '($3 >= 1000) {print $1}' "$1" | while read -r user; do
+	users_groups=$(id -nG "$user" 2>/dev/null)
 	for grp in $GROUPS; do
-		members=$(getent group "$grp" | cut -d':' -f4)
-		echo "$members" | tr ',' '\n' | grep -qx "$user" \
-			echo "$user:$grp"		
+		echo $users_groups | grep -qw "$grp" && echo "$user:$grp"
 	done
-done | sort -u
+done
