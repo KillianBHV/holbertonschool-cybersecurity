676d5ffb559230833aa1f184f4083b62a351209c
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-07 02:07:39 -0400

    Fix Scripts

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -2,7 +2,7 @@
 GROUPS="docker disk shadow"
 
 declare -A GID_TO_GROUPS
-while IFS=':' read -r name _ gid _; do
+while IFS=: read -r name _ gid _; do
 	for target in $GROUPS; do
 		if [ "$name" = "$target" ]; then
 			GID_TO_GROUPS["$gid"]="$name"
@@ -10,13 +10,14 @@ while IFS=':' read -r name _ gid _; do
 	done
 done < <(getent group)
 
-awk -F':' '($3 >= 1000) {print $1 ":" $4}' "$1" | while IFS=':' read -r user gid; do
+awk -F: '($3 >= 1000) {print $1 ":" $4}' "$1" | while IFS=: read -r user gid; do
 	if [ -n "${GID_TO_GROUPS[$gid]}" ]; then
 		echo "$user:${GID_TO_GROUPS[$gid]}"
 	fi
 
 	for grp in $GROUPS; do
 		members=$(getent group "$grp" | cut -d':' -f4)
-		printf '%s\n' "$members" | tr ',' '\n' | grep -qx "$user"	
+		echo "$members" | tr ',' '\n' | grep -qx "$user" \
+			echo "$user:$grp"		
 	done
 done | sort -u
