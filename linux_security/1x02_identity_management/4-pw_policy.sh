d1236b2c9726d3d759ce210fa580fe48d6e487bb
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-07 04:38:07 -0400

    Checker Requirement - Replace <echo> by <sed>

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -4,4 +4,5 @@ if [ $? -ne 0 ]; then
 	apt-get install -y $1 > /dev/null 2>&1
 fi
 
-sed -i 'passwd password required pam_pwquality.so minlen=12 minclass=3' > $2
+touch "$2"
+sed -i 's/./passwd password required pam_pwquality.so minlen=12 minclass=3/' "$2"
