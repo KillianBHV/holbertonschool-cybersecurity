a4212f2f9dba8b82f3aefce23ea427f7c8abd0d6
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-07 04:28:49 -0400

    Checker Requirement - Characters remove

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dpkg -S "$1" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	apt-get install -y "$1" > /dev/null 2>&1
+	apt-get install -y $1 > /dev/null 2>&1
 fi
 
 echo 'passwd  password required		pam_pwquality.so minlen=12 minclass=3' > $2
