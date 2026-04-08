d8a440a2e79327dc798e2607a942292f1755fcf4
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-08 08:49:57 -0400

    Checker Bug - New Test

--- linux_security/1x03_system_visibility/10-recent_logs.sh
+++ linux_security/1x03_system_visibility/10-recent_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while read l; do ts=$(date -d $'\b'"$(echo "$l" | awk '{print $1,$2,$3}')" $'\b' +%s); now=$(date +%s); (( now - ts <= 1800 )) && echo "$l"; done < "$1"
+awk -v now="$(date +%s)" '{print}' "$1"
