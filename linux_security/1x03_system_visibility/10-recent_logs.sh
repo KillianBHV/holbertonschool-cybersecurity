7a287bdde6200db05db71b580e625923dd2d1002
KillianBHV <11841@holbertonstudents.com>
2026-04-08 14:54:40 +0200

    Checker bug FOUND! - Backspace problems

--- linux_security/1x03_system_visibility/10-recent_logs.sh
+++ linux_security/1x03_system_visibility/10-recent_logs.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 awk -v now="$(date +%s)" '{print}' "$1"
+
+#!/bin/bash
+awk -v now="$(date +%s)" '$0 ~ "sshd"{logtime=mktime(sprintf("%s %s %s %s", "2026", $1, $2, $3)); if(now-logtime<=1800) print}' "$1"
