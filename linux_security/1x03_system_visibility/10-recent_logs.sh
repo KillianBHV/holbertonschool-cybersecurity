52ec7d3ccd5b2f83147906578c8e4454bf4997b7
KillianBHV <11841@holbertonstudents.com>
2026-04-08 14:44:51 +0200

    Script Strange Test - Checker Bug

--- linux_security/1x03_system_visibility/10-recent_logs.sh
+++ linux_security/1x03_system_visibility/10-recent_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print}' $1
+while read l; do ts=$(date -d $'\b'"$(echo "$l" | awk '{print $1,$2,$3}')" $'\b' +%s); now=$(date +%s); (( now - ts <= 1800 )) && echo "$l"; done < "$1"
