733452d1df7b2f747a2798460a6d79d1c9822274
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-09 09:37:49 -0400

    Remove now option

--- linux_security/1x04_security_automation/setup_persistence.sh
+++ linux_security/1x04_security_automation/setup_persistence.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cp sentinel.service /etc/systemd/system/
-sudo cp sentinel.timer /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable --now sentinel.timer
+cp sentinel.service /etc/systemd/system/
+cp sentinel.timer /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable sentinel.timer
