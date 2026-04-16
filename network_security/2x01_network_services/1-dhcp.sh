023892d4e7604673f168e82b877b8eb2474fac1d
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-16 07:03:07 -0400

    Checker Requirement - Change Script Method*

--- network_security/2x01_network_services/1-dhcp.sh
+++ network_security/2x01_network_services/1-dhcp.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-grep "option routers" /var/lib/dhcp/dhclient*.leases | tail -n 1 | awk '{print $3}' | tr -d ';'
+#!/bin/bash
+
+grep "dhcp-server-identifier" /var/lib/dhcp/dhclient*.leases 2>/dev/null | tail -n 1 | awk '{print $3}' | tr -d ';'
