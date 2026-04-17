e00bc9f17726538d4f2db1b7f0fef4d5986f0a24
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-17 05:00:59 -0400

    Network ID - Script

---
+++ network_security/2x00_network_fundamentals/5-network_id.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+IFS='.' read -ra ip <<< "$1"; IFS='.' read -ra mask <<< "$2"; for i in 0 1 2 3; do echo -n "" $(( ${ip[i]} & ${mask[i]} )); done | sed 's/^[[:space:]]*//; s/[[:space:]]*$//; s/ /\./g'
