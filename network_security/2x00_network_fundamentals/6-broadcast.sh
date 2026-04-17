f563e77c81dbbe0d09c81694f38e17d2a5bbae0c
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-17 05:21:15 -0400

    Checker Requirement - Replace 255 hexa style by decimal style

--- network_security/2x00_network_fundamentals/6-broadcast.sh
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS='.' read -ra ip <<< "$1"; IFS='.' read -ra mask <<< "$2"; for i in 0 1 2 3; do echo -n "" $(( (${ip[i]} & ${mask[i]}) | ~${mask[i]} & 0xFF )); done | sed 's/^[[:space:]]*//; s/[[:space:]]*$//; s/ /\./g'
+IFS='.' read -ra ip <<< "$1"; IFS='.' read -ra mask <<< "$2"; for i in 0 1 2 3; do echo -n "" $(( (${ip[i]} & ${mask[i]}) | ~${mask[i]} & 255 )); done | sed 's/^[[:space:]]*//; s/[[:space:]]*$//; s/ /\./g'
