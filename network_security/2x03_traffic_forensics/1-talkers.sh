aaf21b43a6a02aee0cfc5bc52898b253766bce1d
Killian BEHAVA <11841@holbertonstudents.com>
2026-04-29 06:22:49 -0400

    Checker Requirement - Remove first awk

--- network_security/2x03_traffic_forensics/1-talkers.sh
+++ network_security/2x03_traffic_forensics/1-talkers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tshark -r "$1" -T fields -e ip.src | awk 'NF' | sort | uniq -c | sort -nr | awk '{print $2}'
+tshark -r "$1" -T fields -e ip.src | sort | uniq -c | sort -rn | awk '{print $2}'
 
