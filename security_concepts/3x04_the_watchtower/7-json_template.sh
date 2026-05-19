cee6cb9ead9c6934446d3def9bae7edc250075a1
KillianBHV <11841@holbertonstudents.com>
2026-05-19 15:05:31 +0200

    Update 7-json_template.sh

--- security_concepts/3x04_the_watchtower/7-json_template.sh
+++ security_concepts/3x04_the_watchtower/7-json_template.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cat > /etc/rsyslog.conf << 'EOF'
-'template(name="json_fmt" type="string" string="{\"time\":\"%timestamp%\", \"host\":\"%hostname%\", \"msg\":\"%msg%\"}\n")'
+'template(name="json_fmt" type="string" string='{"time":"%timestamp%", "host":"%hostname%", "msg":"%msg%"}\n')'
 EOF
 
