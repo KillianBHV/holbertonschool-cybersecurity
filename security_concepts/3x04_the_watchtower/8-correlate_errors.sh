c1b9b7cacfff4a74745bac722875e354981b63eb
KillianBHV <11841@holbertonstudents.com>
2026-05-19 15:20:44 +0200

    Update 8-correlate_errors.sh

--- security_concepts/3x04_the_watchtower/8-correlate_errors.sh
+++ security_concepts/3x04_the_watchtower/8-correlate_errors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for ip in $(grep -E ' 40[34] ' "$1" | awk '{print $1}' | sort | uniq); do
-    count=$(grep "$ip" "$1" | grep -E ' 40[34] ' | wc -l)
+for ip in $(grep -E ' 4[0-9][0-9] ' "$1" | awk '{print $1}' | sort | uniq); do
+    count=$(grep "$ip" "$1" | grep -E ' 4[0-9][0-9] ' | wc -l)
 
     if [ "$count" -gt 5 ]; then
         echo "ALERT: IP $ip is scanning us!"
