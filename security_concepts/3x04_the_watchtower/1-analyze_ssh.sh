7a8ccab9602cdfaec576f4c41287925bfaa9cfeb
KillianBHV <11841@holbertonstudents.com>
2026-05-19 15:12:32 +0200

    Update 1-analyze_ssh.sh

--- security_concepts/3x04_the_watchtower/1-analyze_ssh.sh
+++ security_concepts/3x04_the_watchtower/1-analyze_ssh.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-grep "Failed password" "$1" | grep -Eo '^((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)$' | sort | uniq -c | sort -rn
-
+grep "Failed password" "$1" | grep -Eo 'from ((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)' | awk '{print $2}' | sort | uniq -c | sort -rn
