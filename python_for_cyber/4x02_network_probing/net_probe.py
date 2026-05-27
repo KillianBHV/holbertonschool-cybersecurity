62f1c38c2d2dedcd3ecee54faefaa484dfcbfab1
KillianBHV <11841@holbertonstudents.com>
2026-05-27 05:11:04 -0400

    Test for Checker - Temporary files

---
+++ python_for_cyber/4x02_network_probing/net_probe.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import logging
+
+logger = logging.getLogger()
+
+
+def main():
+    logger.info("NetProbe v1.0 initialized...")
+
+
+if __name__ == '__main__':
+    main()
