f328af421d024fe95fb24aec1b2caccb95b59977
Killian BEHAVA <11841@holbertonstudents.com>
2026-06-04 07:02:49 -0400

    UPDATE sniffer.py

--- python_for_cyber/4x04_network_sniffer/sniffer.py
+++ python_for_cyber/4x04_network_sniffer/sniffer.py
@@ -3,10 +3,23 @@
 import scapy.all as scapy
 
 
-def packet_handler(packet):
+def packet_handler(packet: scapy.Packet) -> None:
     """Get packet summary
     """
-    print(packet.summary())
+    if packet.haslayer(scapy.IP):
+        tcp_check = packet.getlayer(scapy.TCP)
+        udp_check = packet.getlayer(scapy.UDP)
+        icmp_check = packet.getlayer(scapy.ICMP)
+
+        if tcp_check is not None:
+            print("[TCP] ", end='')
+        elif udp_check is not None:
+            print("[UDP] ", end='')
+        elif icmp_check is not None:
+            print("[ICMP] ", end='')
+
+        if tcp_check or udp_check or icmp_check:
+            print(f"{packet[scapy.IP].src} -> {packet[scapy.IP].dst}")
 
 
 if __name__ == '__main__':
