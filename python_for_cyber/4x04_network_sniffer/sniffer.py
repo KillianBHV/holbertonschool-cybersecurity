755430aa17ea4fb1a3b2de9069bc68364d385878
Killian BEHAVA <11841@holbertonstudents.com>
2026-06-04 06:09:50 -0400

    UPDATE sniffer.py

--- python_for_cyber/4x04_network_sniffer/sniffer.py
+++ python_for_cyber/4x04_network_sniffer/sniffer.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print("[INFO] PySniffer initialized.")
+import scapy.all as scapy
+
+
+def packet_hander(packet):
+    """Get packet summary
+    """
+    print(packet.summary())
+
+
+if __name__ == '__main__':
+    print("[INFO] PySniffer initialized.")
+    scapy.sniff(count=5, prn=packet_hander)
