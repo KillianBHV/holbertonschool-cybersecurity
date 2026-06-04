82a0cd02823c027b8b352ca75effaa591a75a49a
Killian BEHAVA <11841@holbertonstudents.com>
2026-06-04 13:19:02 -0400

    TESTS CHECKER BUG

--- python_for_cyber/4x04_network_sniffer/sniffer.py
+++ python_for_cyber/4x04_network_sniffer/sniffer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 
-try:
-    import scapy
-except ModuleNotFoundError:
-    print("Module Not Found!", file=sys.stderr)
-    print([path for path in sys.path])
-    exit(1)
-
+print(os.listdir("/checker"), file=sys.stderr)
 
 print("[INFO] PySniffer initialized")
