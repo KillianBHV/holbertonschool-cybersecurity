845f1ff084ff66a2b7e327659df41fd20b144b37
Killian BEHAVA <11841@holbertonstudents.com>
2026-06-04 13:08:45 -0400

    TESTS CHECKER BUG

--- python_for_cyber/4x04_network_sniffer/sniffer.py
+++ python_for_cyber/4x04_network_sniffer/sniffer.py
@@ -5,7 +5,6 @@ import sys
 try:
     import scapy
 except ModuleNotFoundError:
-    print(hasattr(scapy, 'Packet'), scapy.__version__, file=sys.stderr)
     print("Module Not Found!", file=sys.stderr)
     print([path for path in sys.path])
     exit(1)
