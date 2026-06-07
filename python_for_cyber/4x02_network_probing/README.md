51eb812c940e7fcf9111cb530b174ca2bde45df4
Killian BEHAVA <11841@holbertonstudents.com>
2026-06-07 10:39:20 -0400

    GO BACK - CHECKER BUG - RENEW SCRIPT

--- python_for_cyber/4x02_network_probing/README.md
+++ python_for_cyber/4x02_network_probing/README.md
@@ -1,23 +1,2 @@
-# PYTHON - Network Probing
+# PYTHON - THE NETWORK PROBER
 
----
-
-### 0. The Environment
-Goal: Set up the project environment
-
-Files Solution: **`net_probe.py`**, **`requirements.txt`**, **`venv/`**,
-
-Expected Output:
-```bash
-$ python3 -m venv venv
-$ ls -a
-.  ..  .gitignore  net_probe.py  README.md  requirements.txt  venv/
-$ source venv/bin/activate
-(env)$ ./net_probe.py
-NetProbe v1.0 initialized...
-```
-
-### 0. X
-Goal: Y
-File Solution: Z
-Expected Output: **`None`**
--- python_for_cyber/4x02_network_probing/net_probe.py
+++ python_for_cyber/4x02_network_probing/net_probe.py
@@ -2,61 +2,208 @@
 
 import argparse
 import json
-import socket
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import socket as skt
+import concurrent.futures as crtf
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--target",
+                    help="Target IP",
+                    required=True)
+parser.add_argument("-p", "--ports",
+                    required=True,
+                    help="Port range to scan")
+parser.add_argument("-o", "--output",
+                    help="Generate report to file")
+
+args = parser.parse_args()
 
 
 def check_port(ip: str, port: int) -> bool:
-    """Checks if specified port on ip is open
+    """Checks the availability of a target
+
+    Args:
+        ip: IP Host to test (or hostname)
+        port: Target port
+
+    Returns:
+        True if port is open else False
     """
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
         s.settimeout(1)
-
         s.connect((ip, port))
+
         return True
-    except socket.error:
+    except (OSError, TimeoutError):
         return False
     finally:
-        s.close()
+        if s:
+            s.close()
 
 
-def ping_sweep(subnet: str) -> list:
-    """Scan a specific range of ip on port 80
+def ping_sweep(subnet: str) -> list[str]:
+    """Checks addresses with port 80 open
+
+    Args:
+        subnet: Network section, CIDR is supposed 24
+
+    Returns:
+        List of success IPs test
     """
-    ips = []
-    for k in range(1, 254):
-        ip = f"{subnet}.{k}"
-        print(ip)
-        if check_port(ip, 80):
-            ips.append(ip)
+    list_ip = []
 
-    return ips
+    for e in range(1, 255):
+        address = f"{subnet}.{e}"
+        is_port_open = check_port(address, 8000)
+        if is_port_open:
+            list_ip.append(address)
+
+    return list_ip
 
 
 def get_banner(ip: str, port: int) -> str:
-    """Checks if a banner appears
+    """Extract banner from application-level protocol / service
+
+    Args:
+        ip: Target IP
+        port: Port to scan
+
+    Returns:
+        Banner if succesfully handled else Unknown
     """
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(2)
+        s = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
         s.connect((ip, port))
 
-        s.send(b"HEAD / HTTP/1.0\r\n\r\n")
+        if port == 80:
+            s.send(b'HEAD / HTTP/1.0\r\n\r\n')
+            data = s.recv(1024)
+            if not data:
+                return "Unknown"
 
-        data = s.recv(1024)
-        if not data:
+            banner = data.decode().strip()
+            for line in banner.split('\n'):
+                if "Server" in line.replace('\r', ''):
+                    return line.removeprefix('Server: ').removesuffix('\r')
+
             return "Unknown"
+        elif port == 22:
+            data = s.recv(1024)
+            if not data:
+                return "Unknown"
 
-        return data.decode().strip()
-    except socket.error:
+            banner = data.decode().strip()
+            banner = banner.replace('\r', '')
+            return banner.split()[0]
+        elif port == 21:
+            s.send(f"ftp {ip}".encode())
+            data = s.recv(1024)
+            if not data:
+                return "Unknown"
+
+            banner = data.decode().strip()
+            for line in banner.split('\n'):
+                if line.startswith('220'):
+                    banner = line.removeprefix('220').removesuffix(')')
+                    banner = banner.strip().removeprefix('(').lower()
+                    return banner
+
+            return "Unknown"
+        else:
+            return "Unknown"
+    except OSError:
+        print("[ERROR] Socket process problem occured")
+        return "Unknown"
+    except TimeoutError:
+        print("Socket timeout reached")
         return "Unknown"
     finally:
         if s:
             s.close()
 
 
-def guess_service(port: int) -> str:
+def __scan_single_port(ip: str, port: int) -> dict:
+    """Get one port state
+
+    Args:
+        ip: Target IP
+        port: Single port to scan
+
+    Returns:
+        Metadata dictionary or empty one if port is not open
+    """
+    is_open_port = check_port(ip, port)
+    if is_open_port:
+        banner = get_banner(ip, port)
+        if check_vulnerability(banner):
+            is_vulnerable_banner = 'YES'
+        else:
+            is_vulnerable_banner = 'NO'
+
+        return {
+            'port': port,
+            'state': 'open',
+            'service': banner,
+            'vulnerability': is_vulnerable_banner
+        }
+
+    return {}
+
+
+def scan_ports(ip: str, start_port: int, end_port: int) -> list[dict]:
+    """Scan a range of ports
+
+    Args:
+        ip: Target IP
+        start_port: Lower band port to scan
+        end_port: Lower band port to scan
+
+    Returns:
+        Open ports report with banner grabbing
+    """
+    ports_report = []
+
+    with crtf.ThreadPoolExecutor(max_workers=50) as executor:
+        future_to_port = {
+            executor.submit(__scan_single_port, ip, port):
+                port for port in range(start_port, end_port + 1)
+        }
+
+        for future in crtf.as_completed(future_to_port):
+            scanned_port = future_to_port[future]
+            try:
+                data = future.result()
+            except Exception:
+                print("Error occured!")
+
+            if scanned_port and data:
+                ports_report.append(data)
+
+    print(f"Scanning {ip} from {start_port} to {end_port}...")
+    for port in ports_report:
+        if port:
+            print(
+                f"[+] Port {port['port']} Open: {port['service']} "
+                f"{check_vulnerability(port['service'])}"
+            )
+
+    if args.output:
+        generate_json_report(args.output, ports_report)
+
+    return ports_report
+
+
+def guess_service(ip: str, port: int) -> str:
+    """If banner is Unknown, try to guess service on port
+
+    Args:
+        ip: Target IP
+        port: Port to scan
+
+    Returns:
+        Guessed service or "No Guessed Service"
+    """
     COMMON_PORTS = {
         21: "FTP",
         22: "SSH",
@@ -65,104 +212,68 @@ def guess_service(port: int) -> str:
         3306: "MySQL"
     }
 
-    return COMMON_PORTS.get(port, "Unknown")
+    banner = get_banner(ip, port)
+    if banner == "Unknown":
+        if port in COMMON_PORTS.keys():
+            return f"{COMMON_PORTS.get(port)} (Guessed)"
+        else:
+            return "No Service Guessed"
 
-
-def _scan_single_port(ip: str, port: int) -> dict:
-    """Checks ONE port
-    """
-    if check_port(ip, port):
-        banner = get_banner(ip, port)
-        is_vulnerable = "YES" if check_vulnerability(banner) else "NO"
-
-        print(f"[+] Port {port} Open: {banner} {is_vulnerable}")
-        return {
-            "port": port,
-            "state": "open",
-            "service": f"{banner}",
-            "vulnerability": f"{is_vulnerable}"}
-
-    return None
-
-
-def check_vulnerability(banner: str) -> str:
-    """Search for known bad signatures
-    """
-    MALICIOUS_SIGNATURES = [
-        "vsftpd 2.3.4",
-        "Apache 2.2.8"
-    ]
-
-    for bad in MALICIOUS_SIGNATURES:
-        if bad.lower() in banner.lower():
-            return "[VULNERABLE]"
     return ""
 
 
-def scan_ports(ip: str, start_port: int, end_port: int) -> list:
-    """Get open ports
+def check_vulnerability(banner: str) -> str:
+    """Checks for a potential backdoor with the banner
+
+    Args:
+        banner: The actual banner to test
+
+    Returns:
+        Banner Vulnerability Check
     """
-    ports = []
+    BAD_SIGNS = {
+        "vsftpd 2.3.4",
+        "vsftpd 3.0.5",
+        "Apache 2.2.8"
+    }
 
-    print(f"Scanning {ip} from {start_port} to {end_port}...")
+    for sign in BAD_SIGNS:
+        if banner.lower() in sign.lower():
+            return "[VULNERABLE]"
 
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        futures = []
-
-        for port in range(start_port, end_port+1):
-            future = executor.submit(_scan_single_port, ip, port)
-            futures.append(future)
-
-        for future in as_completed(futures):
-            result = future.result()
-            if result:
-                ports.append(result)
-
-    return ports
+    return ""
 
 
-parser = argparse.ArgumentParser()
+def generate_json_report(filename: str, ports_analytics: list[dict]) -> None:
+    """Generate human-readable report with JSON format
 
-parser.add_argument("-t", "--target", help="Target IP")
-parser.add_argument("-p", "--ports", help="Port range to scan")
-parser.add_argument("-o",
-                    "--output",
-                    required=False,
-                    help="Output JSON file",
-                    default="task_36970_short.json")
+    Args:
+        filename: path or direct filename to write
+        ports_analytics: data to write
+    """
+    to_json_str_data = json.dumps(ports_analytics, indent=2)
+    with open(filename, "w") as file:
+        file.write(to_json_str_data)
+        file.write('\n')
 
-args = parser.parse_args()
 
+def main() -> None:
+    """Program Entry Point"""
+    # print("NetProbe v1.0 initialized...")
+    # print(f"Port 80 is open: {check_port('google.com', 80)}")
+    # print(f"Port 81 is open: {check_port('google.com', 81)}")
 
-def main():
-    print("NetProbe v1.0 initialized...")
-    with open(args.output, 'w') as output:
-        s_ports = scan_ports("scanme.nmap.org", 77, 83)
-        s_ports.append({
-            "port": 4444,
-            "state": "open",
-            "service": "netcat",
-            "vulnerability": "YES"})
-        s_ports.append({
-            "port": 5555,
-            "state": "open",
-            "service": "Shoreline",
-            "vulnerability": "YES"})
-        json.dump(s_ports, output)
+    # print(ping_sweep("192.168.1"))
+    # print(get_banner("scanme.nmap.org", 80))
 
-    with open("task_36970_long.json", 'w') as output:
-        s_ports = scan_ports("scanme.nmap.org", 77, 83)
-        s_ports.append({
-            "port": 4444,
-            "state": "open",
-            "service": "netcat",
-            "vulnerability": "YES"})
-        s_ports.append({
-            "port": 5555,
-            "state": "open",
-            "service": "Shoreline",
-            "vulnerability": "YES"})
-        json.dump(s_ports, output)
+    sep_port = args.ports.find('-')
+    scan_ports(args.target,
+               int(args.ports[:sep_port]),
+               int(args.ports[sep_port + 1:]))
+
+    # scan_ports(args.target, 21, 22)
+    # print(guess_service("192.168.1.28", 80))
+    # print(get_banner("localhost", 21))
 
 
 if __name__ == '__main__':
