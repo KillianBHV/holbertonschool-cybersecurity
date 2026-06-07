a4ad9c44656abf1fb00bf4587b17532b8aab9471
Killian BEHAVA <11841@holbertonstudents.com>
2026-06-07 18:20:47 -0400

    TESTS CHECKER BUG

--- python_for_cyber/4x02_network_probing/net_probe.py
+++ python_for_cyber/4x02_network_probing/net_probe.py
@@ -296,7 +296,7 @@ def main() -> None:
     if args.delay:
         delay = float(args.delay)
 
-    raise Exception(dir())
+    raise Exception(dir(args))
 
     ports = scan_ports(ip, lower_port, upper_port)
     # print_infos(ip, lower_port, upper_port, ports)
