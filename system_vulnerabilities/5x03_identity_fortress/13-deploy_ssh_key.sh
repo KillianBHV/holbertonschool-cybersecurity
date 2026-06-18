b1b1e9022b3044861fabf7d1775440f72dee989f
Killian BEHAVA <11841@holbertonstudents.com>
2026-06-18 14:33:07 -0400

    SSH Key On Target

--- system_vulnerabilities/5x03_identity_fortress/13-deploy_ssh_key.sh
+++ system_vulnerabilities/5x03_identity_fortress/13-deploy_ssh_key.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "usage: You must specify a valid .pub content"
+    exit 1
+fi
+
 echo -e "=== SSH Key Deployment ===\n"
 
 echo "Target user: $(whoami)"
