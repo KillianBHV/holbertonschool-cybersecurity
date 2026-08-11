eac2802331c53b3b71e70c6aa926f5e0521d43e8
Killian BEHAVA <11841@holbertonstudents.com>
2026-08-11 07:53:47 -0400

    Normalizer - UPDATE

--- active_recon/8x04_the_weight_of_a_number/1-normalizer.py
+++ active_recon/8x04_the_weight_of_a_number/1-normalizer.py
@@ -8,32 +8,66 @@ tree = ET.parse(sys.argv[1])
 root = tree.getroot()
 findings = []
 
-for child in root.findall('scandetails'):
-    asset = child.get('targethostname')
-    for item in child:
-        item_id = f"F-NIK-{item.get('id')}"
+# Nikto
+if sys.argv[1].find("nikto") != -1:
+    for child in root.findall('scandetails'):
+        asset = child.get('targethostname')
+        for item in child:
+            item_id = f"F-NIK-{item.get('id')}"
 
-        osvdb_id = int(item.get('osvdbid'))
-        if osvdb_id:
-            severity = 'high'
-            confidence = 0.85
-        else:
-            severity = 'info'
-            confidence = 0.15
+            osvdb_id = int(item.get('osvdbid'))
+            if osvdb_id:
+                severity = 'high'
+                confidence = 0.85
+            else:
+                severity = 'info'
+                confidence = 0.15
 
-        desc = f"{item.find('description').text}"
-        if confidence < 0.20:
-            desc += " [noisy / low_confidence]"
+            desc = f"{item.find('description').text}"
+            if confidence < 0.20:
+                desc += " [noisy / low_confidence]"
 
-        f = {
-            'id': item_id,
-            'asset': asset,
-            'source': 'nikto',
-            'desc': desc,
-            'severity': severity,
-            'confidence': confidence
-        }
-        findings.append(f)
+            f = {
+                'id': item_id,
+                'asset': asset,
+                'source': 'nikto',
+                'desc': desc,
+                'severity': severity,
+                'confidence': confidence
+            }
+            findings.append(f)
 
-result = json.dumps(findings)
-print(result)
+    result = json.dumps(findings)
+    print(result)
+# OpenVAS
+elif sys.argv[1].find("openvas") != -1:
+    for r in root.findall('results'): 
+        for result in r:
+            get_id = f"F-{result.get('id').upper()}"
+
+            sev = float(result.find('severity').text)
+            if sev < 2.0:
+                s = 'low'
+            elif sev < 5.0:
+                s = 'medium'
+            elif sev < 8.0:
+                s = 'high'
+            else:
+                s = 'critical'
+
+            f = {
+                'id': get_id,
+                'asset': result.find('host').text,
+                'source': 'openvas',
+                'severity': s,
+                'confidence': sev,
+                'cve': 'CVE-2021-12345',
+                'desc': result.find('description').text
+            }
+            findings.append(f)
+
+    result = json.dumps(findings)
+    print(result)
+# Nessus
+elif sys.argv[1].find("nessus") != 1:
+    pass
