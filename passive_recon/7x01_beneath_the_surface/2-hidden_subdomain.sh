458cf95068849a781be3e782fd3573a1cc772309
Killian BEHAVA <11841@holbertonstudents.com>
2026-09-25 08:19:02 -0400

    UPDATE Script

--- passive_recon/7x01_beneath_the_surface/2-hidden_subdomain.sh
+++ passive_recon/7x01_beneath_the_surface/2-hidden_subdomain.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for w in $(grep -E "internal|backend|admin" /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-5000.txt); do sub="$w.astralis-cloud.example"; if [ -n "$(dig +short "$sub")" ]; then echo "$sub"; break; fi; sleep 0.2; done
+for w in $(grep -E "internal|backend|admin" /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-5000.txt); do sub="$w.astralis-cloud.example"; if [ -n "$(dig +short "$sub")" ]; then echo "$sub"; break; fi; sleep 0.2; done # head -1
