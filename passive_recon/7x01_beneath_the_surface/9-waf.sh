cbdd8b0812311b499feaabd11bad67df86332191
Killian BEHAVA <11841@holbertonstudents.com>
2026-09-25 10:56:44 -0400

    UPDATE Script

--- passive_recon/7x01_beneath_the_surface/9-waf.sh
+++ passive_recon/7x01_beneath_the_surface/9-waf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-if [ -z $(wafw00f -a https://portal.astralis-cloud.example 2>/dev/null | grep "WAF detected" | grep -Eo "[0-9]+") ]; then curl -I https://portal.astralis-cloud.example 2>/dev/null | awk '/CF-RAY:/ {print "Cloudfare"}'; fi
+if [ -z $(wafw00f -a https://portal.astralis-cloud.example 2>/dev/null | grep "WAF detected" | grep -Eo "[0-9]+") ]; then curl -I https://portal.astralis-cloud.example 2>/dev/null | awk '/CF-RAY:/ {print "Cloudfare"}'; fi | head -1
