d583396bb1a05b3306dbcc2c4557133db7c2de75
Killian BEHAVA <11841@holbertonstudents.com>
2026-09-25 10:12:42 -0400

    Script

---
+++ passive_recon/7x01_beneath_the_surface/6-san_entry.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+openssl s_client -connect portal.astralis-cloud.example:443 </dev/null 2>/dev/null | openssl x509 -noout -ext subjectAltName | awk -F, '/DNS:/ {print $3}' | sed -E 's/[[:space:]]|\r//'
