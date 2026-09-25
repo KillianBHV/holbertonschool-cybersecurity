7cf3520f6e0ba0ae4f3aa49eb7fa3e4998b73055
Killian BEHAVA <11841@holbertonstudents.com>
2026-09-25 09:46:03 -0400

    Script UPDATE

--- passive_recon/7x01_beneath_the_surface/4-admin_port.sh
+++ passive_recon/7x01_beneath_the_surface/4-admin_port.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -sT -T2 --scan-delay 150ms --max-rate 20 --open -p49152-65535 admin.astralis-cloud.example | grep -E '^[0-9]+/' | awk -F'/' '{print $1}'| head -1
+nmap -sT -T2 --scan-delay 150ms --max-rate 20 --open -p49152-65535 admin.astralis-cloud.example | grep -E '^[0-9]+/' | awk -F/ '{print $1}'| head -1
