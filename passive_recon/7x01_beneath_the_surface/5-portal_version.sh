19a87fca72b63243dc1f8e4384b2fdf1bb562ea1
Killian BEHAVA <11841@holbertonstudents.com>
2026-09-25 09:19:16 -0400

    UPDATE Script

--- passive_recon/7x01_beneath_the_surface/5-portal_version.sh
+++ passive_recon/7x01_beneath_the_surface/5-portal_version.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sI https://portal.astralis-cloud.example 2>/dev/null | awk '/^Server:/ { sub(/\r$/, ""); print $2}'
+curl -sI https://portal.astralis-cloud.example 2>/dev/null | awk '/^Server:/ { sub(/\r$/, ""); print $2}' | head -1
